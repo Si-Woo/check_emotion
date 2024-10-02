34ceffc39cb47dabe26074054069401042194974
yi-shuu <shu@iqua.co.kr>
2024-10-02 12:57:04 +0900

    update

--- lib/home.dart
+++ lib/home.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         actions: [
           GestureDetector(
               onTap: () {
